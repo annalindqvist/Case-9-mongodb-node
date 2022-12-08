a0220e96acf926f839c957d218d920af754a7454
annalindqvist <annalindqvist_@live.se>
2022-12-08 14:34:15 +0100

    new icons and setintervall

--- public/js/code.js
+++ public/js/code.js
@@ -120,35 +120,7 @@ function handleLike(id) {
         .catch((err) => console.log(err));
 }
 
-newDate("Tue Dec 06 2022 12:43:52 GMT+0100 (centraleuropeisk normaltid)")
-
-function newDate(date) {
-
-    let dateOfPost = new Date(date);
-    let day = dateOfPost.getDate();
-    let month = dateOfPost.getMonth();
-    let correctDate = day + " " + month;
-    console.log(correctDate)
-    return dateOfPost;
-}
-
-
-// function showFlash() {
-
-//     let element = document.getElementsByClassName("flash-message");
-//     for (var i = 0; i < element.length; i++) {
-//         element[i].classList.add('visibleFlash');
-//         setTimeout = ((i) => { element[i].classList = element[i].classList.replace("visibleFlash", ""); }, 4000);
-//     }
-
-//     // setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-//     // setTimeout = (() => { flashContainer.classList.remove("visibleFlash"); }, 4000);
-
-// }
-
 function timeSince(date) {
-    
-    console.log("date", date)
 
     if (typeof date !== 'object') {
         date = new Date(date);
@@ -159,7 +131,7 @@ function timeSince(date) {
 
     let interval = Math.floor(seconds / 31536000);
     if (interval >= 1) {
-        intervalType = 'year';
+        intervalType = 'y';
     } else {
         interval = Math.floor(seconds / 2592000);
         if (interval >= 1) {
@@ -167,32 +139,28 @@ function timeSince(date) {
         } else {
             interval = Math.floor(seconds / 86400);
             if (interval >= 1) {
-                intervalType = 'day';
+                intervalType = 'd';
             } else {
                 interval = Math.floor(seconds / 3600);
                 if (interval >= 1) {
-                    intervalType = "hour";
+                    intervalType = "h";
                 } else {
                     interval = Math.floor(seconds / 60);
                     if (interval >= 1) {
-                        intervalType = "minute";
+                        intervalType = "m";
                     } else {
                         interval = seconds;
-                        intervalType = "second";
+                        intervalType = "s";
                     }
                 }
             }
         }
     }
-
-    if (interval > 1 || interval === 0) {
-        intervalType += 's';
-    }
-
+    
     // console.log(interval + ' ' + intervalType);
-    return interval + ' ' + intervalType;
+    return interval + ''+ intervalType;
 };
 
 let aDay = 24 * 60 * 60 * 1000;
-console.log("timeSince test", timeSince(new Date("Tue Dec 08 2022 09:43:52 GMT+0100 (centraleuropeisk normaltid)")));
+
 
