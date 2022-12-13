badb63cfe01dcdbd7455fec8155fa041f1083b2e
annalindqvist <annalindqvist_@live.se>
2022-12-13 10:56:03 +0100

    removed port from .env

--- configs.js
+++ configs.js
@@ -4,10 +4,9 @@ import dotenv from 'dotenv';
 const config = dotenv.config().parsed;
 
 // individual exports
-const PORT = Number(config.PORT);
 const SESSION_SECRET = config.SESSION_SECRET;
 const SESSION_MAXAGE = Number(config.SESSION_MAXAGE); 
 const MONGODB_URL = config.MONGODB_URL;
-const MONGODB_NAME = config.MONGODB_NAME;
 
-export { config, PORT, SESSION_SECRET, SESSION_MAXAGE, MONGODB_URL, MONGODB_NAME };
+
+export { config, SESSION_SECRET, SESSION_MAXAGE, MONGODB_URL };
